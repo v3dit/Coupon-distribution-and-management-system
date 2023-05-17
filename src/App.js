741678bcd62d7ee1a8b1aa703f3c476786023f4f
v3dit <veditbeladia@gmail.com>
2023-05-17 22:04:45 +0530

    navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
-import './App.css';
-import {Card, CardImg, CardImgOverlay, CardTitle, CardText } from 'reactstrap';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  CardText,
+} from 'reactstrap';
 
-function App() {
+function App(args) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
   return (
     <div className="App">
       <div>
-        <Card>
+        <Navbar {...args}>
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="me-auto" navbar>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Login Option
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem><NavLink to="/admin">Customer</NavLink></DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>HR</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Admin</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <NavLink href="/components/">Instagram</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  Location
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <NavbarText>By Ettarra</NavbarText>
+          </Collapse>
+        </Navbar>
+      </div>
+      <div>
+        <Card className='m-5 '>
           <CardImg
             alt="Card image cap"
             src="https://picsum.photos/900/270?grayscale"
